f45bec4e6892a50d0a1f30f21dd4c1ec0ff82781
Gustavarendd <111903425+Gustavarendd@users.noreply.github.com>
2023-02-08 19:49:22 +0100

    added shop and category pages and prod from DB

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,4 @@
-import CategoryItem from "../category-item/category-item.component";
-
+import DirectoryItem from "../directory-item/directory-item.component";
 const Directory = () => {
   const categories = [
     {
@@ -31,7 +30,7 @@ const Directory = () => {
   return (
     <div className="directory-container">
       {categories.map((category) => (
-        <CategoryItem category={category} key={category.id} />
+        <DirectoryItem category={category} key={category.id} />
       ))}
     </div>
   );
