6ebcfd1b29eb7fab1f0e0b2d8dde9e25d114eca0
Gustavarendd <111903425+Gustavarendd@users.noreply.github.com>
2023-02-19 19:45:14 +0100

    no log

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -41,8 +41,6 @@ const PaymentForm = () => {
       paymentIntent: { client_secret },
     } = response;
 
-    console.log(client_secret);
-
     const paymentResult = await stripe.confirmCardPayment(client_secret, {
       payment_method: {
         card: elements.getElement(CardElement),
